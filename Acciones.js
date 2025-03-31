9872ea4c395e2f1615857d586410b5372918dc13
maravirc <91441469+maravirc@users.noreply.github.com>
2025-03-31 16:32:21 -0500

    Update Acciones.js

--- Acciones.js
+++ Acciones.js
@@ -1,6 +1,6 @@
 // Función para redirigir al formulario
 function redirigir() {
-    window.location.href = "https://20.51.224.205/Formularios/Formulario.html";
+    window.location.href = "https://20.51.224.205/Domingos/Formulario.html";
 }
 
 // Simulando progreso
